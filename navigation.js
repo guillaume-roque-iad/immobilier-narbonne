009b863d39d03dd9423eb7ddd0858279a5ac804a
guillaume-roque-iad <guillaume.roque@iadfrance.fr>
2026-09-16 20:20:37 +0200

    Forcer le chargement du nouveau design Propertips

--- navigation.js
+++ navigation.js
@@ -46,7 +46,7 @@
   document.head.appendChild(script);
 
   const media = document.createElement('script');
-  media.src = '/propertips-media.js?v=20260916h';
+  media.src = '/propertips-media.js?v=20260916j';
   media.defer = true;
   document.head.appendChild(media);
 })();
