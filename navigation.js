4d7075e2a38368913f5acdba05716f95415c1b7e
guillaume-roque-iad <guillaume.roque@iadfrance.fr>
2026-09-16 20:02:13 +0200

    Bump Propertips media cache version

--- navigation.js
+++ navigation.js
@@ -46,7 +46,7 @@
   document.head.appendChild(script);
 
   const media = document.createElement('script');
-  media.src = '/propertips-media.js?v=20260916e';
+  media.src = '/propertips-media.js?v=20260916f';
   media.defer = true;
   document.head.appendChild(media);
 })();
